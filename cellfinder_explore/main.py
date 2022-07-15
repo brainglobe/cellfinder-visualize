026683f3b0bbd309bbb08ce06c667645fb5d0fd9
stephenlenzi <stephen.lenzi@gmail.com>
2022-07-15 11:14:01 +0100

    bug fix entry point for real

--- cellfinder_explore/main.py
+++ cellfinder_explore/main.py
@@ -3,50 +3,54 @@ import pathlib
 import fire
 
 from cellfinder_explore.process_summary import plot_cellfinder_bar_summary
-from cellfinder_explore.region_groupings import region_dict
 from cellfinder_explore.render import render_areas
+from magicgui import magicgui
 
 
-def analyse(experiment_dir,
-            output_dir=None,
-            coronal_slice_position=None,
-            slice_thickness=1000,
+@magicgui(experiment_dir={'mode': 'd'}, output_dir={'mode': 'd'}, call_button="Run", persist=False)
+def analyse(experiment_dir=pathlib.Path.home(),
+            output_dir=pathlib.Path.home(),
+            coronal_slice_start=0,
+            coronal_slice_end=12000,
             root=True,
             show_reference_structures=True,
             filter_cells_by_structure=False,
             hemisphere='left',
             slice_root=False,
             downsample_factor=5,
-            highlight_subregion=None,
+            highlight_subregion=5,
+            region_list= ["VISp", "VISpor","VISli","VISl","VISal","VISrl","VISam","VISpm","CP"],
+            colors= ["#e41a1c", "#377eb8","#4daf4a","#984ea3","#ff7f00","#ffff33","#a65628","#f781bf","#999999"],
+            reference_region ="CTX",
             ):
     experiment_dir = pathlib.Path(experiment_dir)
     points_files = list(experiment_dir.rglob('points*'))
     summary_files = list(experiment_dir.rglob('summary*'))
     additional_obj_files = list(experiment_dir.rglob('*.obj'))
 
-    for reference_region, region_list in region_dict.items():
-        plot_cellfinder_bar_summary(
-            summary_files, region_list, reference_region, output_dir, lateralisation=hemisphere
+    plot_cellfinder_bar_summary(
+        summary_files, region_list, reference_region, output_dir, lateralisation=hemisphere,colors=colors,
+    )
+    if experiment_dir is not None:
+        render_areas(
+            points_files,
+            region_list,
+            colors=colors,
+            coronal_slice=coronal_slice_start,
+            slice_thickness=coronal_slice_end,
+            root=root,
+            show_reference_structures=show_reference_structures,
+            filter_cells_by_structure=filter_cells_by_structure,
+            hemisphere=hemisphere,
+            slice_root=slice_root,
+            downsample_factor=downsample_factor,
+            highlight_subregion=highlight_subregion,
+            additional_obj_files=additional_obj_files,
         )
-        if experiment_dir is not None:
-            render_areas(
-                points_files,
-                region_list,
-                coronal_slice=coronal_slice_position,
-                slice_thickness=slice_thickness,
-                root=root,
-                show_reference_structures=show_reference_structures,
-                filter_cells_by_structure=filter_cells_by_structure,
-                hemisphere=hemisphere,
-                slice_root=slice_root,
-                downsample_factor=downsample_factor,
-                highlight_subregion=highlight_subregion,
-                additional_obj_files=additional_obj_files,
-            )
 
 
 def main():
-    fire.Fire(analyse)
+    analyse.show(run=True)
 
 
 if __name__ == "__main__":
